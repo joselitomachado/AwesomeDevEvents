149f7803b94a110818f33e2f9f8b977b5dd2d379
Joselito Machado <opaguelosantos@gmail.com>
2024-05-05 12:24:31 -0300

    first commit

---
+++ AwesomeDevEvents.API/Entities/DevEventSpeaker.cs
@@ -0,0 +1,10 @@
+﻿namespace AwesomeDevEvents.API.Entities;
+
+public class DevEventSpeaker
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string TalkTitle { get; set; } = string.Empty;
+    public string TalkDescription { get; set; } = string.Empty;
+    public string LinkedInProfile { get; set; } = string.Empty;
+}
