de5d91759b0e825dcafb6e0f45911d3742122e80
Joselito Machado <opaguelosantos@gmail.com>
2024-05-09 14:03:13 -0300

    feat: mapeamentos de objetos

---
+++ AwesomeDevEvents.API/Models/DevEventViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AwesomeDevEvents.API.Models;
+
+public class DevEventViewModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public List<DevEventSpeakerViewModel> Speakers { get; set; } = [];
+}
