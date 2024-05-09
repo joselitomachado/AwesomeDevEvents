94ba9dade5b8fa7f5415412ee5438046211e74fc
Joselito Machado <opaguelosantos@gmail.com>
2024-05-09 14:03:05 -0300

    feat: mapeamentos de objetos

---
+++ AwesomeDevEvents.API/Models/DevEventSpeakerInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AwesomeDevEvents.API.Models;
+
+public class DevEventSpeakerInputModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string TalkTitle { get; set; } = string.Empty;
+    public string TalkDescription { get; set; } = string.Empty;
+    public string LinkedInProfile { get; set; } = string.Empty;
+}
